42ffbf50d25008110d4fa89c5b52668e634e8cfe
Junbo ZHAO <zhaojunbo@intern.douban.com>
2014-05-10 14:24:29 +0800

    add in a verify module

---
+++ verify/create_csv.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import sys
+import os.path
+
+# This is a tiny script to help you creating a CSV file from a face
+# database with a similar hierarchie:
+#
+#  philipp@mango:~/facerec/data/at$ tree
+#  .
+#  |-- README
+#  |-- s1
+#  |   |-- 1.pgm
+#  |   |-- ...
+#  |   |-- 10.pgm
+#  |-- s2
+#  |   |-- 1.pgm
+#  |   |-- ...
+#  |   |-- 10.pgm
+#  ...
+#  |-- s40
+#  |   |-- 1.pgm
+#  |   |-- ...
+#  |   |-- 10.pgm
+#
+
+
+def create_csv(*args):
+    if len(args) != 2:
+        print "usage: create_csv(<base_path>, <path_to_csv>)"
+        sys.exit(1)
+
+    BASE_PATH = args[0]
+    SEPARATOR = ";"
+    FILE_NAME = args[1]
+
+    label = 0
+    fid = open(FILE_NAME, 'w')
+    for dirname, dirnames, filenames in os.walk(BASE_PATH):
+        for subdirname in dirnames:
+            subject_path = os.path.join(dirname, subdirname)
+            for filename in os.listdir(subject_path):
+                abs_path = "%s/%s" % (subject_path, filename)
+                #print "%s%s%d" % (abs_path, SEPARATOR, label)
+                fid.write("%s%s%d\n" % (abs_path, SEPARATOR, label))
+            label = label + 1
---
+++ verify/del_img.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import random
+
+
+def del_img(*args):
+    if len(args) != 2:
+        print "usage: del(<path>, n_delete)"
+        sys.exit(1)
+    n_del = args[1]
+    assert isinstance(n_del, int), "plz enter a interger"
+    assert n_del > 0 and n_del < 10, "Each folder only has 10 .pgm files"
+    print "We will delete %i images in each folder" % n_del
+    thedir = os.path.abspath(args[0])
+    folder_name = [os.path.join(thedir, name) for name in os.listdir(thedir)
+                   if os.path.isdir(os.path.join(thedir, name))]
+    for _folder in folder_name:
+        files = os.listdir(_folder)
+        random.shuffle(files)
+        files = files[1:n_del+1]
+        for _file in files:
+            assert _file.endswith('.pgm')
+            os.remove(os.path.join(_folder, _file))
